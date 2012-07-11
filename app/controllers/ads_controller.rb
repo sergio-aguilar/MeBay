8153da07a63cea51063f156703d84e1b8b8db344
Sergio Aguilar <aguiserg@gmail.com>
2012-07-10 23:03:18 -0500

    Work on New/Edit forms

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -16,4 +16,14 @@ class AdsController < ApplicationController
     @ad.save
     redirect_to @ad, notice: "Ad was successfully created."
   end
+  
+  def edit
+    @ad = Ad.find(params[:id])    
+  end
+  
+  def update
+    @ad = Ad.find(params[:id])
+    @ad.update_attributes(params[:ad])
+    redirect_to @ad, notice: "Ad was successfully updated"
+  end
 end
